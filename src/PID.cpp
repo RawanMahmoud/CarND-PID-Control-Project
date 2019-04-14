20550c2ff2934dca8193e160c88e65833506f5cc
RawanMahmoud <rawan.gamaleldin@gmail.com>
2019-04-14 05:10:13 +0000

    first commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -12,6 +12,13 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
 
 }
 
@@ -19,12 +26,18 @@ void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
+  double prev_cte = p_error;
 
+  p_error = cte;
+  d_error = cte-prev_cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double total_error = -Kp*p_error -Kd*d_error -Ki*i_error;
+
+  return total_error;  // TODO: Add your total error calc here!
 }
